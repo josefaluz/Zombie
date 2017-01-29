d8c0c2527ef60b3a7e1104bb96eb0d92eee56830
LOPEZ-1 <LOPEZ-1@LOPEZ-1-PC>
2017-01-28 21:34:43 -0500

    1/28/17 9:33pm Phila, 11:33pm Salta Agregado game, maps, ship, y modificado el main (ZombieSurvivalIsladn.java)

--- zombieSurvivalIsland/src/zombiesurvivalisland/ZombieSurvivalIsland.java
+++ zombieSurvivalIsland/src/zombiesurvivalisland/ZombieSurvivalIsland.java
@@ -5,7 +5,10 @@
  */
 package zombiesurvivalisland;
 
+import byui.cit260.zombieSurvivalIsland.model.Game;
+import byui.cit260.zombieSurvivalIsland.model.Maps;
 import byui.cit260.zombieSurvivalIsland.model.Player;
+import byui.cit260.zombieSurvivalIsland.model.Ship;
 
 /**
  *
@@ -25,6 +28,33 @@ public class ZombieSurvivalIsland {
       String playerInfo = playerOne.toString();
       System.out.println(playerInfo);
      
+      Game gameOne = new Game();
+      
+      gameOne.setTotalTime(7);
+      gameOne.setNoPlayers(2);
+      gameOne.setLastSession(1);
+      
+      String gameInfo = gameOne.toString();
+      System.out.println(gameInfo);
+      
+      Maps mapsOne = new Maps();
+      
+      mapsOne.setRowCount(5);
+      mapsOne.setColumnCount(5);
+      
+      String mapsInfo = mapsOne.toString();
+      System.out.println(mapsInfo);
+      
+      Ship shipOne = new Ship();
+      
+      shipOne.setDescription("Use this ship to get back home.");
+      shipOne.setWidth(15);
+      shipOne.setHeight(30);
+      shipOne.setLenght(70);
+      shipOne.setCapacity(1);
+      
+      String shipInfo = shipOne.toString();
+      System.out.println(shipInfo);
       
     }
     
