dccee618bdc987e5c00a4fadea2f6625c05351d0
LOPEZ-1 <LOPEZ-1@LOPEZ-1-PC>
2017-02-09 23:24:44 -0500

    Revert "Incluye initializeMap"
    This reverts commit df9a02d1a6e67d808bbb40662775c5223ea397e1.

--- zombieSurvivalIsland/src/byui/cit260/zombieSurvivalIsland/control/GameControl2.java
+++ zombieSurvivalIsland/src/byui/cit260/zombieSurvivalIsland/control/GameControl2.java
@@ -33,23 +33,7 @@ byte message=  level;
         return message;
     }
 
-   public String initializeMap( String column, int row) {
-        
-       String columns = "abcdefghi";
-       
-       if (!columns.contains(column)){
-               return "-1";
-   }
-       
-       if (row < 1 || row > 11) {
-       
-           return "-1";
-        }
-       
-       String coordinate =  column + row; 
-        return  coordinate;
-       
-   }
+   
 
 }
     
